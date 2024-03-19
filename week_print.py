3bfbc7e778af0187d6995bb32ee1298eaa19f8d7
ADRIBHATTACHARYA <163902918+ADRIBHATTACHARYA@users.noreply.github.com>
2024-03-19 19:33:22 +0530

    Create week_print.py

---
+++ week_print.py
@@ -0,0 +1,18 @@
+date=int(input("Enter the number between 1 to 7"))
+#To print the days of a week
+if date == 1:
+    print("Its a sunday\n")
+elif date == 2:
+    print("Its a Monday\n")
+elif date == 3:
+    print("Its a Tuesday\n")
+elif date == 4:
+    print("Its a Wednesday\n")
+elif date == 5:
+    print("Its a Thursday \n")
+elif date == 6:
+    print("Its a Friday\n")
+elif date == 7:
+    print("Its a Saturday\n")
+else:
+    print("Day number is Invalid")
