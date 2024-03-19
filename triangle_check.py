bf301e507456c2cb2116c66a6c7a35f8db521aac
ADRIBHATTACHARYA <163902918+ADRIBHATTACHARYA@users.noreply.github.com>
2024-03-19 19:35:15 +0530

    Create triangle_check.py

---
+++ triangle_check.py
@@ -0,0 +1,10 @@
+side_1=int(input("Enter the side 1 of the triangle"))
+side_2=int(input("Enter the side 2 of the triangle"))
+side_3=int(input("Enter the side 3 of the triangle"))
+
+#To basically check if it is triangle or not
+if ((side_1+ side_2) > side_3):
+   print ("It is a triangle")
+else :
+   print("It is not a triangle")
+
